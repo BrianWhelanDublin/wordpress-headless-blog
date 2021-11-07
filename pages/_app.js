009e8f932107270ad4b76263fb2878b1652e38ae
Brian Whelan <brian@friday.ie>
2021-11-07 22:38:44 +0000

    add apollo provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import Layout from "../components/Layout"
 import "../styles/main.scss"
 import Nav from "../components/Nav"
+import { ApolloProvider } from "@apollo/client"
+import client from "../lib/apollo"
 
 const MyApp = ({ Component, pageProps }) => {
     return (
-        <Layout>
-            <Nav />
-            <Component {...pageProps} />
-        </Layout>
+        <ApolloProvider client={client} >
+            <Layout>
+                <Nav />
+                <Component {...pageProps} />
+            </Layout>
+        </ApolloProvider >
     )
 }
 
